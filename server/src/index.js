c35d333b7ac1919e7e32aafa5b62a9813888a30b
ben <zhenben.toh@silverlakeaxis.com>
2024-09-12 23:55:59 +0800

    added navbar and links

---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from 'cors';
+import mongooese from 'mongoose';
+import { userRouter } from './routes/users.js';
+ 
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+
+mongooese.connect(
+    "mongodb+srv://priadmin:admin1234@recipes.n1jda.mongodb.net/recipes?retryWrites=true&w=majority&appName=Recipes",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+app.listen(3001, () => console.log("SERVER STARTED!"));
